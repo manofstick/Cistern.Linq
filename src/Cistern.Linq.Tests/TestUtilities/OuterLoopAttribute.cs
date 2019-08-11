f592dfe9fd589fe109ca897bb7f06a3bc802c169
Paul Westcott <p.westcott@gmail.com>
2019-08-11 12:43:57 +1000

    Copied corefx's System.Linq tests, modified for Cistern

---
+++ src/Cistern.Linq.Tests/TestUtilities/OuterLoopAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Tests.TestUtilities
+{
+    class OuterLoopAttribute : Attribute
+    {
+        public OuterLoopAttribute(string ignore) { }
+    }
+}
