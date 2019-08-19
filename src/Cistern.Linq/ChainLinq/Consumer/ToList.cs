801ac4c038f346d7c79a52fb82caffa7f28a0da8
Paul Westcott <p.westcott@gmail.com>
2019-08-19 18:25:10 +1000

    Added optimizations on ToList

--- src/Cistern.Linq/ChainLinq/Consumer/ToList.cs
+++ src/Cistern.Linq/ChainLinq/Consumer/ToList.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Cistern.Linq.ChainLinq.Consumer
 {
-    sealed class ToList<T> : Consumer<T, List<T>>
+    sealed class ToList<T>
+        : Consumer<T, List<T>>
+        , Optimizations.ITailEnd<T>
     {
         public ToList() : base(new List<T>()) { }
 
@@ -12,6 +15,56 @@ namespace Cistern.Linq.ChainLinq.Consumer
         {
             Result.Add(input);
             return ChainStatus.Flow;
+        }
+
+        void Optimizations.ITailEnd<T>.Select<S>(ReadOnlySpan<S> source, Func<S, T> selector)
+        {
+            foreach (var input in source)
+            {
+                Result.Add(selector(input));
+            }
+        }
+
+        ChainStatus Optimizations.ITailEnd<T>.SelectMany<TSource, TCollection>(TSource source, ReadOnlySpan<TCollection> span, Func<TSource, TCollection, T> resultSelector)
+        {
+            foreach (var input in span)
+            {
+                Result.Add(resultSelector(source, input));
+            }
+            return ChainStatus.Flow;
+        }
+
+        void Optimizations.ITailEnd<T>.Where(ReadOnlySpan<T> source, Func<T, bool> predicate)
+        {
+            foreach(var input in source)
+            {
+                if (predicate(input))
+                {
+                    Result.Add(input);
+                }
+            }
+        }
+
+        void Optimizations.ITailEnd<T>.Where<Enumerator>(Optimizations.ITypedEnumerable<T, Enumerator> source, Func<T, bool> predicate)
+        {
+            foreach (var input in source)
+            {
+                if (predicate(input))
+                {
+                    Result.Add(input);
+                }
+            }
+        }
+
+        void Optimizations.ITailEnd<T>.WhereSelect<S>(ReadOnlySpan<S> source, Func<S, bool> predicate, Func<S, T> selector)
+        {
+            foreach (var input in source)
+            {
+                if (predicate(input))
+                {
+                    Result.Add(selector(input));
+                }
+            }
         }
     }
 }
