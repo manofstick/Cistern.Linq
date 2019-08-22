242feeda198834bb53503d7a2dc97687a86861f3
Turnerj <git@turnerj.com>
2019-08-22 12:21:02 +0930

    Added take benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/TakeBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+	[CoreJob, MemoryDiagnoser]
+	public class TakeBenchmark : NumericBenchmarkBase
+	{
+		[Benchmark(Baseline = true)]
+		public double[] SystemLinq()
+		{
+			return System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Take(Numbers, NumberOfItems / 2));
+		}
+		
+		[Benchmark]
+		public double[] CisternLinq()
+		{
+			return Enumerable.ToArray(Enumerable.Take(Numbers, NumberOfItems / 2));
+		}
+	}
+}
