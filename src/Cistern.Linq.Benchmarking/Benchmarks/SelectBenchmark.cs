df50af47caf9932cd51b89d055e9f7e294f998e1
Turnerj <git@turnerj.com>
2019-08-22 18:11:31 +0930

    Added select benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/SelectBenchmark.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+	[CoreJob, MemoryDiagnoser]
+	public class SelectBenchmark : NumericBenchmarkBase
+	{
+		[Benchmark]
+		public double[] ForLoop()
+		{
+			var items = new double[NumberOfItems];
+			foreach (var n in Numbers)
+			{
+				items[(int)n - 1] = n + 1;
+			}
+			return items;
+		}
+
+		[Benchmark(Baseline = true)]
+		public double[] SystemLinq()
+		{
+			var items = new double[NumberOfItems];
+			foreach (var item in System.Linq.Enumerable.Select(Numbers, n => n + 1))
+			{
+				items[(int)item - 2] = item;
+			}
+			return items;
+		}
+		
+		[Benchmark]
+		public double[] CisternLinq()
+		{
+			var items = new double[NumberOfItems];
+			foreach (var item in Enumerable.Select(Numbers, n => n + 1))
+			{
+				items[(int)item - 2] = item;
+			}
+			return items;
+		}
+	}
+}
