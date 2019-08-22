d7d54c3fb62878f2f43d1904484b6f9913d1ce4a
Turnerj <git@turnerj.com>
2019-08-22 16:56:45 +0930

    Added average benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/AverageBenchmark.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+	[CoreJob, MemoryDiagnoser]
+	public class AverageBenchmark : NumericBenchmarkBase
+	{
+		[Benchmark]
+		public double ForLoop()
+		{
+			var count = 0;
+			double sum = 0;
+			foreach (var n in Numbers)
+			{
+				sum += n;
+				count++;
+			}
+			
+			if (count == 0)
+			{
+				throw new InvalidOperationException("Sequence contains no elements");
+			}
+
+			return sum / count;
+		}
+
+		[Benchmark(Baseline = true)]
+		public double SystemLinq()
+		{
+			return System.Linq.Enumerable.Average(Numbers);
+		}
+		
+		[Benchmark]
+		public double CisternLinq()
+		{
+			return Enumerable.Average(Numbers);
+		}
+	}
+}
