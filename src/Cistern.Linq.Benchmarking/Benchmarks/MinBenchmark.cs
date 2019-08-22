b6bcfc5a22dd08cfb04a14505ca031840b68c9c6
Turnerj <git@turnerj.com>
2019-08-22 16:51:41 +0930

    Added Min benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/MinBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+	[CoreJob, MemoryDiagnoser]
+	public class MinBenchmark : NumericBenchmarkBase
+	{
+		[Benchmark]
+		public double ForLoop()
+		{
+			double? min = 0;
+			foreach (var n in Numbers)
+			{
+				if (n < min || min == null)
+				{
+					min = n;
+				}
+			}
+			
+			if (!min.HasValue)
+			{
+				throw new InvalidOperationException("Sequence has no elements!");
+			}
+			
+			return min.Value;
+		}
+
+		[Benchmark(Baseline = true)]
+		public double SystemLinq()
+		{
+			return System.Linq.Enumerable.Min(Numbers);
+		}
+		
+		[Benchmark]
+		public double CisternLinq()
+		{
+			return Enumerable.Min(Numbers);
+		}
+	}
+}
