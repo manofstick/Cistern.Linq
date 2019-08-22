58824117a1d3e03d5fa4e72fbede9d5e090ad038
Turnerj <git@turnerj.com>
2019-08-22 16:50:53 +0930

    Added Max benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/MaxBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+	[CoreJob, MemoryDiagnoser]
+	public class MaxBenchmark : NumericBenchmarkBase
+	{
+		[Benchmark]
+		public double ForLoop()
+		{
+			double? max = 0;
+			foreach (var n in Numbers)
+			{
+				if (n > max || max == null)
+				{
+					max = n;
+				}
+			}
+			
+			if (!max.HasValue)
+			{
+				throw new InvalidOperationException("Sequence has no elements!");
+			}
+			
+			return max.Value;
+		}
+
+		[Benchmark(Baseline = true)]
+		public double SystemLinq()
+		{
+			return System.Linq.Enumerable.Max(Numbers);
+		}
+		
+		[Benchmark]
+		public double CisternLinq()
+		{
+			return Enumerable.Max(Numbers);
+		}
+	}
+}
