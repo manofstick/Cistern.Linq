57a99706bfefcf33cb992f34a32d224c5905647a
Paul Westcott <p.westcott@gmail.com>
2019-08-24 06:33:03 +1000

    ToList optimizations

--- src/Cistern.Linq/ChainLinq/Consumer/ToList.cs
+++ src/Cistern.Linq/ChainLinq/Consumer/ToList.cs
@@ -5,6 +5,7 @@ namespace Cistern.Linq.ChainLinq.Consumer
 {
     sealed class ToList<T>
         : Consumer<T, List<T>>
+        , Optimizations.IHeadStart<T>
         , Optimizations.ITailEnd<T>
     {
         public ToList() : base(new List<T>()) { }
@@ -17,65 +18,130 @@ namespace Cistern.Linq.ChainLinq.Consumer
             return ChainStatus.Flow;
         }
 
-        void Optimizations.ITailEnd<T>.Select<S>(ReadOnlySpan<S> source, Func<S, T> selector)
+        private void EnsureCapacity(int length)
         {
+            Result.Capacity = Result.Count + length;
+        }
+
+        private void EnsureCapacity(int? maybeLength)
+        {
+            if (maybeLength.HasValue)
+            {
+                Result.Capacity = Result.Count + maybeLength.Value;
+            }
+        }
+
+        void Optimizations.IHeadStart<T>.Execute(ReadOnlySpan<T> source)
+        {
+            var result = Result;
+
+            EnsureCapacity(source.Length);
             foreach (var input in source)
             {
-                Result.Add(selector(input));
+                result.Add(input);
             }
+
+            Result = result;
+        }
+
+        void Optimizations.IHeadStart<T>.Execute<Enumerator>(Optimizations.ITypedEnumerable<T, Enumerator> source)
+        {
+            var result = Result;
+
+            EnsureCapacity(source.TryLength);
+            foreach (var input in source)
+            {
+                result.Add(input);
+            }
+
+            Result = result;
+        }
+
+        void Optimizations.ITailEnd<T>.Select<S>(ReadOnlySpan<S> source, Func<S, T> selector)
+        {
+            var result = Result;
+
+            EnsureCapacity(source.Length);
+            foreach (var input in source)
+            {
+                result.Add(selector(input));
+            }
+
+            Result = result;
         }
 
         ChainStatus Optimizations.ITailEnd<T>.SelectMany<TSource, TCollection>(TSource source, ReadOnlySpan<TCollection> span, Func<TSource, TCollection, T> resultSelector)
         {
+            var result = Result;
+
+            EnsureCapacity(span.Length);
             foreach (var input in span)
             {
-                Result.Add(resultSelector(source, input));
+                result.Add(resultSelector(source, input));
             }
+
+            Result = result;
             return ChainStatus.Flow;
         }
 
         void Optimizations.ITailEnd<T>.Where(ReadOnlySpan<T> source, Func<T, bool> predicate)
         {
-            foreach(var input in source)
+            var result = Result;
+
+            foreach (var input in source)
             {
                 if (predicate(input))
                 {
-                    Result.Add(input);
+                    result.Add(input);
                 }
             }
+
+            Result = result;
         }
 
         void Optimizations.ITailEnd<T>.Where<Enumerator>(Optimizations.ITypedEnumerable<T, Enumerator> source, Func<T, bool> predicate)
         {
+            var result = Result;
+
             foreach (var input in source)
             {
                 if (predicate(input))
                 {
-                    Result.Add(input);
+                    result.Add(input);
                 }
             }
+
+            Result = result;
         }
 
         void Optimizations.ITailEnd<T>.WhereSelect<S>(ReadOnlySpan<S> source, Func<S, bool> predicate, Func<S, T> selector)
         {
+            var result = Result;
+
             foreach (var input in source)
             {
                 if (predicate(input))
                 {
-                    Result.Add(selector(input));
+                    result.Add(selector(input));
                 }
             }
+
+            Result = result;
         }
 
         void Optimizations.ITailEnd<T>.WhereSelect<Enumerator, S>(Optimizations.ITypedEnumerable<S, Enumerator> source, Func<S, bool> predicate, Func<S, T> selector)
         {
+            var result = Result;
+
             foreach (var input in source)
             {
                 if (predicate(input))
                 {
-                    Result.Add(selector(input));
+                    result.Add(selector(input));
                 }
             }
+
+            Result = result;
         }
     }
 }
