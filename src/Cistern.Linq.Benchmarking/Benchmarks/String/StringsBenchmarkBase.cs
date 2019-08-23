119304ef02d4682295b9988b50486a4b01dd8314
Paul Westcott <p.westcott@gmail.com>
2019-08-24 09:34:06 +1000

    Added sorted/random to words benchmark

--- src/Cistern.Linq.Benchmarking/Benchmarks/String/StringsBenchmarkBase.cs
+++ src/Cistern.Linq.Benchmarking/Benchmarks/String/StringsBenchmarkBase.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using System;
 
 namespace Cistern.Linq.Benchmarking.Benchmarks
 {
@@ -6,6 +7,9 @@ namespace Cistern.Linq.Benchmarking.Benchmarks
 	{
         const string source = "https://github.com/dwyl/english-words/raw/042fb7abca733b186f150dcce85023048e84705f/words.txt";
         const string filename = "lotsofwords.txt";
+
+        [Params(true, false)]
+        public bool Sorted;
 
         [Params(10, 1000, 466544)]
 		public int WordsCount;
@@ -20,6 +24,23 @@ namespace Cistern.Linq.Benchmarking.Benchmarks
                 new System.Net.WebClient().DownloadFile(source, filename);
             }
             Words = System.IO.File.ReadAllLines(filename);
+
+            if (Sorted)
+            {
+                Words = Words.OrderBy(x => x).ToArray();
+            }
+            else
+            {
+                // deterministric "random" shuffle...
+                var r = new Random(42);
+                for (var i = 0; i < Words.Length; ++i)
+                {
+                    var j = r.Next(i, Words.Length - 1);
+                    var tmp = Words[i];
+                    Words[i] = Words[j];
+                    Words[j] = tmp;
+                }
+            }
 
             Words.Take(WordsCount).ToArray();
 		}
