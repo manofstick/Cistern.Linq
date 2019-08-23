ca6631f43825983168d41daa0b265cdcf562c14c
Paul Westcott <p.westcott@gmail.com>
2019-08-24 08:49:20 +1000

    Added new string test

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/String/GroupByCharCharChar.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+using DesiredShape = System.Collections.Generic.Dictionary<(char, char, char), System.Collections.Generic.List<string>>;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks.String
+{
+    [CoreJob, MemoryDiagnoser]
+    public class GroupByCharCharChar : StringsBenchmarkBase
+    {
+        [Benchmark]
+        public DesiredShape ForLoop()
+        {
+            var answer = new DesiredShape();
+
+            foreach (var n in Words)
+            {
+                if (n.Length >= 3)
+                {
+                    if (!answer.TryGetValue((n[0], n[1], n[2]), out var words))
+                        words = new List<string>();
+                    words.Add(n);
+                }
+            }
+
+            return answer;
+        }
+
+        [Benchmark(Baseline = true)]
+        public DesiredShape SystemLinq()
+        {
+            return
+                System.Linq.Enumerable.ToDictionary(
+                    System.Linq.Enumerable.GroupBy(
+                        System.Linq.Enumerable.Where(Words, w => w.Length >= 3),
+                            w => (w[0], w[1], w[2])),
+                                ws => ws.Key,
+                                ws => System.Linq.Enumerable.ToList(ws));
+        }
+
+        [Benchmark]
+        public DesiredShape CisternLinq()
+        {
+            return
+                Words
+                .Where(w => w.Length >= 3)
+                .GroupBy(w => (w[0], w[1], w[2]))
+                .ToDictionary(ws => ws.Key, ws => ws.ToList());
+        }
+    }
+}
