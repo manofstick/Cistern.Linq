1c036f1b91265e7df53d51730ad84155c4f38138
Paul Westcott <p.westcott@gmail.com>
2019-08-25 08:19:59 +1000

    Added semi-realistic 'customer data' benchmark

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/Containers/ContainersBase.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using Cistern.Linq.Benchmarking.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks
+{
+    public abstract class ContainersBase
+    {
+        [ParamsSource(nameof(ValuesForCustomers))]
+        public object CustomersData { get; set; }
+
+        public IEnumerable<DummyData.Customer> Customers => (IEnumerable<DummyData.Customer>)CustomersData;
+
+        private static IEnumerable<T> AsRealEnumerable<T>(IEnumerable<T> data)
+        {
+            foreach (var x in data)
+                yield return x;
+        }
+
+        public IEnumerable<IEnumerable<DummyData.Customer>> ValuesForCustomers => new IEnumerable<DummyData.Customer>[] {
+            (IEnumerable<DummyData.Customer>)DummyData.GetCustomers().ToArray(),
+            (IEnumerable<DummyData.Customer>)DummyData.GetCustomers().ToList(),
+            (IEnumerable<DummyData.Customer>)AsRealEnumerable(DummyData.GetCustomers()),
+            (IEnumerable<DummyData.Customer>)DummyData.GetCustomers().ToImmutableArray(),
+            (IEnumerable<DummyData.Customer>)DummyData.GetCustomers().ToImmutableList(),
+            (IEnumerable<DummyData.Customer>)Microsoft.FSharp.Collections.ListModule.OfSeq(DummyData.GetCustomers()),
+        };
+
+		[GlobalSetup]
+		public void Setup()
+		{
+            Immutable.Register.RegisterSystemCollectionsImmutable();
+            FSharp.Register.RegisterFSharpCollections();
+        }
+	}
+}
---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/Containers/SelectWhereToListBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+using DesiredShape = System.Collections.Generic.List<string>;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks.Containers
+{
+    [CoreJob, MemoryDiagnoser]
+    public class SelectWhereToListBenchmark : ContainersBase
+    {
+        [Benchmark]
+        public DesiredShape ForLoop()
+        {
+            var subset = new List<string>();
+            foreach (var c in Customers)
+            {
+                if (c.DOB.Year < 1980)
+                {
+                    subset.Add(c.Name);
+                }
+            }
+            return subset;
+        }
+
+        [Benchmark(Baseline = true)]
+        public DesiredShape SystemLinq()
+        {
+            return 
+                System.Linq.Enumerable.ToList(
+                    System.Linq.Enumerable.Select(
+                        System.Linq.Enumerable.Where(
+                            Customers,
+                            c => c.DOB.Year < 1980),
+                        c => c.Name)
+                    );
+        }
+
+        [Benchmark]
+        public DesiredShape CisternLinq()
+        {
+            return 
+                Customers
+                .Where(c => c.DOB.Year < 1980)
+                .Select(c => c.Name)
+                .ToList();
+        }
+
+    }
+}
