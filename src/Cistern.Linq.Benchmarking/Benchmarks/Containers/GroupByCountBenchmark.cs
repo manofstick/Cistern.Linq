afb5d4322bf57a56ec1c3a8b6752f4b4552d7ac4
Paul Westcott <p.westcott@gmail.com>
2019-08-25 10:13:06 +1000

    Added new GroupByCount test

---
+++ src/Cistern.Linq.Benchmarking/Benchmarks/Containers/GroupByCountBenchmark.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+
+using DesiredShape = System.Collections.Generic.List<(string State, int Count)>;
+
+namespace Cistern.Linq.Benchmarking.Benchmarks.Containers
+{
+    [CoreJob, MemoryDiagnoser]
+    public class GroupByCountBenchmark : ContainersBase
+    {
+        [Benchmark]
+        public DesiredShape ForLoop()
+        {
+            var counts = new Dictionary<string, int>();
+            foreach (var c in Customers)
+            {
+                counts.TryGetValue(c.State, out var count);
+                counts[c.State] = count + 1;
+            }
+
+            var results = new DesiredShape();
+            foreach (var c in counts)
+                results.Add((c.Key, c.Value));
+            return results;
+        }
+
+        [Benchmark(Baseline = true)]
+        public DesiredShape SystemLinq()
+        {
+            return 
+                System.Linq.Enumerable.ToList(
+                    System.Linq.Enumerable.Select(
+                        System.Linq.Enumerable.GroupBy(
+                            Customers,
+                            c => c.State),
+                        c => (c.Key, c.Count()))
+                    );
+        }
+
+        [Benchmark]
+        public DesiredShape CisternLinq()
+        {
+            return
+                Customers
+                .GroupBy(c => c.State)
+                .Select(c => (c.Key, c.Count()))
+                .ToList();
+        }
+
+    }
+}
