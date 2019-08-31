758daf32db93ebbe64da372417d9e7827e72cf8b
Paul Westcott <p.westcott@gmail.com>
2019-08-31 16:21:04 +1000

    Updated max benchmark

--- src/Cistern.Linq.Benchmarking/Benchmarks/Numeric/MaxBenchmark.cs
+++ src/Cistern.Linq.Benchmarking/Benchmarks/Numeric/MaxBenchmark.cs
@@ -4,23 +4,23 @@ using System;
 namespace Cistern.Linq.Benchmarking.Benchmarks.Numeric
 {
     /*
-    |      Method | NumberOfItems |          Mean |      Error |     StdDev | Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
-    |------------ |-------------- |--------------:|-----------:|-----------:|------:|--------:|-------:|------:|------:|----------:|
-    |     ForLoop |             0 |            NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
-    |  SystemLinq |             0 |            NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
-    | CisternLinq |             0 |            NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
-    |             |               |               |            |            |       |         |        |       |       |           |
-    |     ForLoop |             1 |     0.9256 ns |  0.0140 ns |  0.0131 ns |  0.04 |    0.00 |      - |     - |     - |         - |
-    |  SystemLinq |             1 |    22.8922 ns |  0.1329 ns |  0.1243 ns |  1.00 |    0.00 | 0.0102 |     - |     - |      32 B |
-    | CisternLinq |             1 |    33.9663 ns |  0.2416 ns |  0.2260 ns |  1.48 |    0.01 | 0.0101 |     - |     - |      32 B |
-    |             |               |               |            |            |       |         |        |       |       |           |
-    |     ForLoop |            10 |     6.3473 ns |  0.0667 ns |  0.0624 ns |  0.09 |    0.00 |      - |     - |     - |         - |
-    |  SystemLinq |            10 |    74.4951 ns |  0.5109 ns |  0.4779 ns |  1.00 |    0.00 | 0.0101 |     - |     - |      32 B |
-    | CisternLinq |            10 |    39.0711 ns |  0.1636 ns |  0.1450 ns |  0.52 |    0.00 | 0.0101 |     - |     - |      32 B |
-    |             |               |               |            |            |       |         |        |       |       |           |
-    |     ForLoop |          1000 |   655.3799 ns |  3.6851 ns |  3.4470 ns |  0.12 |    0.00 |      - |     - |     - |         - |
-    |  SystemLinq |          1000 | 5,528.3815 ns | 24.7642 ns | 23.1645 ns |  1.00 |    0.00 | 0.0076 |     - |     - |      32 B |
-    | CisternLinq |          1000 |   610.2671 ns |  3.8098 ns |  3.1814 ns |  0.11 |    0.00 | 0.0095 |     - |     - |      32 B |     
+    |      Method | NumberOfItems |         Mean |      Error |     StdDev | Ratio | RatioSD |  Gen 0 | Gen 1 | Gen 2 | Allocated |
+    |------------ |-------------- |-------------:|-----------:|-----------:|------:|--------:|-------:|------:|------:|----------:|
+    |     ForLoop |             0 |           NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
+    |  SystemLinq |             0 |           NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
+    | CisternLinq |             0 |           NA |         NA |         NA |     ? |       ? |      - |     - |     - |         - |
+    |             |               |              |            |            |       |         |        |       |       |           |
+    |     ForLoop |             1 |     1.608 ns |  0.0190 ns |  0.0178 ns |  0.07 |    0.00 |      - |     - |     - |         - |
+    |  SystemLinq |             1 |    22.973 ns |  0.1100 ns |  0.1029 ns |  1.00 |    0.00 | 0.0102 |     - |     - |      32 B |
+    | CisternLinq |             1 |    33.620 ns |  0.2093 ns |  0.1958 ns |  1.46 |    0.01 | 0.0101 |     - |     - |      32 B |
+    |             |               |              |            |            |       |         |        |       |       |           |
+    |     ForLoop |            10 |    10.296 ns |  0.0517 ns |  0.0483 ns |  0.14 |    0.00 |      - |     - |     - |         - |
+    |  SystemLinq |            10 |    74.339 ns |  0.2752 ns |  0.2439 ns |  1.00 |    0.00 | 0.0101 |     - |     - |      32 B |
+    | CisternLinq |            10 |    38.664 ns |  0.2495 ns |  0.2334 ns |  0.52 |    0.00 | 0.0101 |     - |     - |      32 B |
+    |             |               |              |            |            |       |         |        |       |       |           |
+    |     ForLoop |          1000 |   783.662 ns | 15.5524 ns | 26.4091 ns |  0.14 |    0.01 |      - |     - |     - |         - |
+    |  SystemLinq |          1000 | 5,518.438 ns | 17.5211 ns | 15.5320 ns |  1.00 |    0.00 | 0.0076 |     - |     - |      32 B |
+    | CisternLinq |          1000 |   611.927 ns |  3.7129 ns |  3.4730 ns |  0.11 |    0.00 | 0.0095 |     - |     - |      32 B |
     */
 
     [CoreJob, MemoryDiagnoser]
@@ -34,16 +34,20 @@ namespace Cistern.Linq.Benchmarking.Benchmarks.Numeric
                 throw new InvalidOperationException("Sequence contains no elements");
             }
 
-            double max = 0;
-			foreach (var n in Numbers)
+            double max = double.NaN;
+            var idx = 0;
+			for(; idx < Numbers.Length && double.IsNaN(max); ++idx)
 			{
-				if (n > max)
-				{
-					max = n;
-				}
+				max = Numbers[idx];
 			}
-			
-			return max;
+            for(; idx < Numbers.Length; ++idx)
+            {
+                var n = Numbers[idx];
+                if (n > max)
+                    max = n;
+            }
+
+            return max;
 		}
 
 		[Benchmark(Baseline = true)]
--- src/Cistern.Linq.Benchmarking/Benchmarks/Numeric/NumericBenchmarkBase.cs
+++ src/Cistern.Linq.Benchmarking/Benchmarks/Numeric/NumericBenchmarkBase.cs
@@ -3,21 +3,20 @@
 namespace Cistern.Linq.Benchmarking.Benchmarks.Numeric
 {
     public abstract class NumericBenchmarkBase
-	{
+    {
+        [Params(0, 1, 10, 1000)]
+        public int NumberOfItems;
 
-		[Params(0, 1, 10, 1000)]
-		public int NumberOfItems;
+        public double[] Numbers;
 
-		public double[] Numbers;
-
-		[GlobalSetup]
-		public void Setup()
-		{
-			Numbers = new double[NumberOfItems];
-			for (int i = 0; i < NumberOfItems; i++)
-			{
-				Numbers[i] = i + 1;
-			}
-		}
-	}
+        [GlobalSetup]
+        public void Setup()
+        {
+            Numbers = new double[NumberOfItems];
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                Numbers[i] = i + 1;
+            }
+        }
+    }
 }
