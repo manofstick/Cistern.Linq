abd42e7978359bb17eb19902fff4f503afcbdbbd
Paul Westcott <p.westcott@gmail.com>
2019-09-03 17:42:55 +1000

    Added RangeEnumerable

--- src/Cistern.Linq/ChainLinq/Consume/Range.cs
+++ src/Cistern.Linq/ChainLinq/Consume/Range.cs
@@ -1,12 +1,77 @@
-﻿namespace Cistern.Linq.ChainLinq.Consume
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cistern.Linq.ChainLinq.Consume
 {
     static class Range
     {
+        struct RangeEnumerator : IEnumerator<int>
+        {
+            private readonly int End;
+
+            public RangeEnumerator(int start, int count)
+            {
+                Current = unchecked(start - 1);
+                End = unchecked(start + count - 1);
+            }
+
+            public int Current { get; private set; }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (Current == End)
+                    return false;
+
+                Current++;
+                return true;
+
+            }
+
+            public void Reset() => throw new System.NotImplementedException();
+        }
+
+        struct RangeEnumerable
+            : Optimizations.ITypedEnumerable<int, RangeEnumerator>
+        {
+            private readonly int Count;
+            private readonly int Start;
+
+            public RangeEnumerable(int start, int count)
+            {
+                Start = start;
+                Count = count;
+            }
+
+            public IEnumerable<int> Source => null;
+
+            public int? TryLength => Count;
+
+            public RangeEnumerator GetEnumerator() => new RangeEnumerator(Start, Count);
+
+            public bool TryGetSourceAsSpan(out ReadOnlySpan<int> readOnlySpan)
+            {
+                readOnlySpan = default;
+                return false;
+            }
+        }
+
         public static void Invoke(int start, int count, Chain<int> chain)
         {
             try
             {
-                Pipeline(start, count, chain);
+                if (chain is Optimizations.IHeadStart<int> optimized)
+                {
+                    optimized.Execute<RangeEnumerable, RangeEnumerator>(new RangeEnumerable(start, count));
+                }
+                else
+                {
+                    Pipeline(start, count, chain);
+                }
                 chain.ChainComplete();
             }
             finally
