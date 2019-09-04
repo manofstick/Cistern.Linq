f68b0ac4cd0751815541034344d8837d18a6fea2
Paul Westcott <p.westcott@gmail.com>
2019-09-04 15:42:41 +1000

    Added unfold via GetEnumerable benchmark

--- src/Cistern.Linq.Benchmarking.FSharp/Unfold.fs
+++ src/Cistern.Linq.Benchmarking.FSharp/Unfold.fs
@@ -7,6 +7,12 @@ type UnfoldFSharpListBase () =
     [<Params(0, 1, 10, 1000)>]
     member val NumberOfItems = 0 with get, set
 
+    member this.folder x =
+        if x < this.NumberOfItems
+        then Some (float x, x+1)
+        else None
+
+
 (*
 |  Method | NumberOfItems |         Mean |      Error |     StdDev | Ratio | RatioSD |
 |-------- |-------------- |-------------:|-----------:|-----------:|------:|--------:|
@@ -25,13 +31,41 @@ type UnfoldFSharpListBase () =
 type UnfoldFSharpList_Max() =
     inherit UnfoldFSharpListBase ()
 
-    member private __.folder x =
-        if x < base.NumberOfItems
-        then Some (float x, x+1)
-        else None
 
     [<Benchmark (Baseline = true)>]
     member this.Seq () = Seq.unfold this.folder 0 |> Seq.max
 
     [<Benchmark>]
     member this.Cistern () =  Linq.unfold this.folder 0 |> Linq.max
+
+(*
+|  Method | NumberOfItems |         Mean |       Error |      StdDev | Ratio | RatioSD |
+|-------- |-------------- |-------------:|------------:|------------:|------:|--------:|
+|     Seq |             0 |     56.36 ns |   0.5472 ns |   0.4851 ns |  1.00 |    0.00 |
+| Cistern |             0 |     89.20 ns |   0.4938 ns |   0.4377 ns |  1.58 |    0.01 |
+|         |               |              |             |             |       |         |
+|     Seq |             1 |     77.39 ns |   0.5153 ns |   0.4820 ns |  1.00 |    0.00 |
+| Cistern |             1 |    115.28 ns |   1.0779 ns |   1.0083 ns |  1.49 |    0.02 |
+|         |               |              |             |             |       |         |
+|     Seq |            10 |    296.87 ns |   2.0273 ns |   1.8963 ns |  1.00 |    0.00 |
+| Cistern |            10 |    364.74 ns |   1.6430 ns |   1.5369 ns |  1.23 |    0.01 |
+|         |               |              |             |             |       |         |
+|     Seq |          1000 | 23,108.10 ns |  79.7722 ns |  74.6190 ns |  1.00 |    0.00 |
+| Cistern |          1000 | 26,190.79 ns | 117.5903 ns | 109.9941 ns |  1.13 |    0.01 |
+*)
+type UnfoldFSharpList_MaxByGetEnumerable() =
+    inherit UnfoldFSharpListBase ()
+
+    let max (unfold:seq<float>) =
+        let mutable max = System.Double.MinValue
+        for x in unfold do
+            if x > max then
+                max <- x
+        max
+
+    [<Benchmark (Baseline = true)>]
+    member this.Seq () = Seq.unfold this.folder 0 |> max
+
+    [<Benchmark>]
+    member this.Cistern () = Linq.unfold this.folder 0 |> max
+
