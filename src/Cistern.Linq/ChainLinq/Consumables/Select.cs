86e60d8ef7966f821932869aeb620bdba4eb8662
Paul Westcott <p.westcott@gmail.com>
2019-09-11 16:06:37 +1000

    Split ConsumableEnumerator out to own file

--- src/Cistern.Linq/ChainLinq/Consumables/Select.cs
+++ src/Cistern.Linq/ChainLinq/Consumables/Select.cs
@@ -4,40 +4,6 @@ using System.Collections.Generic;
 
 namespace Cistern.Linq.ChainLinq.Consumables
 {
-    abstract class ConsumableEnumerator<V> : ConsumableCons<V>, IEnumerable<V>, IEnumerator<V>
-    {
-        private readonly int _threadId;
-        internal int _state;
-        internal V _current;
-
-        protected ConsumableEnumerator()
-        {
-            _threadId = Environment.CurrentManagedThreadId;
-        }
-
-        V IEnumerator<V>.Current => _current;
-        object IEnumerator.Current => _current;
-
-        void IEnumerator.Reset() => ThrowHelper.ThrowNotSupportedException();
-
-        public virtual void Dispose()
-        {
-            _state = int.MaxValue;
-            _current = default(V);
-        }
-
-        public override IEnumerator<V> GetEnumerator()
-        {
-            ConsumableEnumerator<V> enumerator = _state == 0 && _threadId == Environment.CurrentManagedThreadId ? this : Clone();
-            enumerator._state = 1;
-            return enumerator;
-        }
-
-        internal abstract ConsumableEnumerator<V> Clone();
-
-        public abstract bool MoveNext();
-    }
-
     sealed partial class SelectArray<T, U>
         : ConsumableEnumerator<U>
         , Optimizations.IMergeSelect<U>
