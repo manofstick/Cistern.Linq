aece0722ba595e960f9bd4a577dab1de5ac57e94
Paul Westcott <p.westcott@gmail.com>
2019-09-21 15:49:35 +1000

    missed adding new files

---
+++ src/Cistern.Linq.FSharp/Links/chunkBySize.fs
@@ -0,0 +1,38 @@
+﻿namespace Cistern.Linq.FSharp.Links
+
+open Cistern.Linq.ChainLinq
+
+type ChunkBySizeActivity<'T>(chunkBySize, next) =
+    inherit Cistern.Linq.ChainLinq.Activity<'T,array<'T>>(next)
+
+    let mutable current = Unchecked.defaultof<_>
+    let mutable idx = Unchecked.defaultof<_>
+
+    override __.ProcessNext input =
+        if isNull current then
+            current <- Array.zeroCreate chunkBySize
+            idx <- 0
+
+        current.[idx] <- input
+        idx <- idx + 1
+
+        if idx = current.Length then
+            let output = current
+            current <- Unchecked.defaultof<_>
+            base.Next output
+        else
+            Cistern.Linq.ChainLinq.ChainStatus.Filter
+            
+    override __.ChainComplete status =
+        if ProcessNextResultHelper.IsStopped status || isNull current then
+            base.ChainComplete status
+        else
+            System.Array.Resize (&current, idx)
+            base.Next current |> ignore
+            current <- Unchecked.defaultof<_>
+            base.ChainComplete ChainStatus.Flow
+
+type ChunkBySize<'T>(chunkBySize:int) =
+    interface Cistern.Linq.ChainLinq.ILink<'T, array<'T>> with
+        member __.Compose activity = 
+            upcast ChunkBySizeActivity(chunkBySize, activity)
