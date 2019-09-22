c6eddbc2cecbfc1df8104860d16e920b20ba9051
Paul Westcott <p.westcott@gmail.com>
2019-09-22 15:57:12 +1000

    Removed commented code that doesn't compile due to https://github.com/dotnet/fsharp/issues/7607

--- src/Cistern.Linq.FSharp/Links/chooseV.fs
+++ src/Cistern.Linq.FSharp/Links/chooseV.fs
@@ -1,7 +1,6 @@
 ﻿namespace Cistern.Linq.FSharp.Links
 
 open Cistern.Linq.ChainLinq
-open System
 open System.Runtime.CompilerServices
 
 [<Sealed>]
@@ -15,23 +14,6 @@ type ChooseVActivity<'T,'U>(chooser:'T->ValueOption<'U>, next) =
         | ValueSome item -> base.Next item
 
     override this.ProcessNext input = this.Process input
-
-(*
-    interface Optimizations.IHeadStart<'T> with
-        member this.Execute<'Enumerable, 'Enumerator 
-                                when 'Enumerable :> Optimizations.ITypedEnumerable<'T, 'Enumerator> 
-                                and 'Enumerator :> System.Collections.Generic.IEnumerator<'T>
-                            >(source:'Enumerable) : ChainStatus = 
-            use mutable e = source.GetEnumerator ()
-            while e.MoveNext () do
-                this.Process e.Current |> ignore
-            ChainStatus.Flow
-
-        member this.Execute (source:ReadOnlySpan<'T>) : ChainStatus =
-            for input in source do
-                this.Process input |> ignore
-            ChainStatus.Flow
-*)
 
 type ChooseV<'T,'U>(chooser:'T -> ValueOption<'U>) =
     interface Cistern.Linq.ChainLinq.ILink<'T, 'U> with
